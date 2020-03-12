d630999da7f8a327adbdb60e0dfdc6dbd7fa6e4c
SANAN AKTHER <61096842+sananakther9740@users.noreply.github.com>
2020-03-13 00:58:14 +0530

    Update instainsane.sh

--- instainsane.sh
+++ instainsane.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Coded by: thelinuxchoice (Don't change, noob!)
-# www.github.com/thelinuxchoice
-# www.instagram.com/linux_choice
+# Coded by: sananakther9740
+# www.github.com/sananakther9740
 
 trap 'store; exit 1;' 2
 
@@ -36,7 +35,7 @@ printf "\e[1;95m | || '_ \ / __|| __| / _\` |   \e[0m\e[1;91m/ /\/| '_ \ / __| /
 printf "\e[1;95m | || | | |\__ \| |_ | (_| |\e[0m\e[1;91m/\/ /_  | | | |\__ \| (_| || | | ||  __/ \e[0m\n"
 printf "\e[1;77m |_||_| |_||___/ \__| \__,_|\e[0m\e[1;77m\____/  |_| |_||___/ \__,_||_| |_| \___| \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[41m  Instagram Brute Forcer v1.0, Author: @thelinuxchoice (Github/IG)  \e[0m\n" #Don't change, noob
+printf "\e[1;77m\e[41m  Instagram Brute Forcer v1.0, Author: @sananakther9740 (Github)  \e[0m\n"
 printf "\n"
 }
 
